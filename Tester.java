00e89320c20dad4738264707cdd55534d0b16895
Olivier Piotrowski <oliwierpiotrowski97@gmail.com>
2026-05-15 14:50:00 +0200

    Dodano klasę Tester

---
+++ Tester.java
@@ -0,0 +1,49 @@
+public class Tester extends Pracownik {
+
+    private boolean czyAutomatyzujacy;
+    private int liczbaScenariuszy;
+
+    public Tester(String idPracownika,
+                  String imie,
+                  String nazwisko,
+                  double stawkaBazowa,
+                  boolean czyAutomatyzujacy,
+                  int liczbaScenariuszy) {
+
+        super(idPracownika, imie, nazwisko, stawkaBazowa);
+
+        this.czyAutomatyzujacy = czyAutomatyzujacy;
+        this.liczbaScenariuszy = liczbaScenariuszy;
+    }
+
+    @Override
+    public double obliczKosztMiesieczny() {
+
+        if (czyAutomatyzujacy) {
+            return getStawkaBazowa() + 1500;
+        }
+
+        return getStawkaBazowa();
+    }
+
+    @Override
+    public String przedstawSie() {
+        return "Tester " + getImie() +
+                ", scenariusze testowe: " + liczbaScenariuszy;
+    }
+
+    public void uruchomRaportTestow() {
+        System.out.println("Uruchamianie raportu testów...");
+    }
+
+    public boolean isCzyAutomatyzujacy() {
+        return czyAutomatyzujacy;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                " | Tester | Automatyzujący: " + czyAutomatyzujacy +
+                " | Scenariusze: " + liczbaScenariuszy;
+    }
+}
